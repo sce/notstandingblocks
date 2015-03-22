8ca44e4451fcf90c0126dca109aa21f2785eb938
S. Christoffer Eliesen <christoffer@eliesen.no>
2015-03-22 16:00:30 +0100

    sce.drivers/test1: Add testBlit.

--- sce.drivers/test/test1.cpp
+++ sce.drivers/test/test1.cpp
@@ -63,11 +63,61 @@ int testInput() {
   return 0;
 }
 
-void printHelp() {
+void inputLoop(GraphicDriver *gd) {
+  int action;
+  do {
+    if (gd) gd->flip();
+    action = Input::getAction(true);
+  } while(action != Input::SHUTDOWN and action != Input::EXITSTATE);
+}
+
+// - bring up window
+// - run input fetch loop
+// - quit on user request
+int testBlit() {
+  cout << "testBlit." << endl;
+
+  GraphicDriver gd;
+  gd.setupScreen(640, 480, 0, SDL_WINDOW_FULLSCREEN);
+
+  SDL_Texture *background = gd.loadImage("../../nsb/resources/pics/background1.png");
+  if (background)
+    gd.blitToMain(background, 0, 0);
+
+  SDL_Texture *img1 = gd.loadImage("../../nsb/resources/pics/round1a2.png");
+  if (background)
+    gd.blitToMain(img1, 100, 100);
+
+  SDL_Texture *img2 = gd.loadImage("../../nsb/resources/pics/round2a.png");
+  if (background)
+    gd.blitToMain(img2, 200, 200);
+
+  SDL_Texture *img3 = gd.loadImage("../../nsb/resources/pics/round3a.png");
+  if (background) {
+    gd.blitToMain(img3, 300, 300);
+    gd.blitToMain(img3, 400, 300);
+    gd.blitToMain(img3, 500, 300);
+  }
+
+  //gd.flip();
+  inputLoop(&gd);
+
+  SDL_DestroyTexture(img1);
+  SDL_DestroyTexture(img2);
+  SDL_DestroyTexture(img3);
+
+  cout << "Done." << endl;
+
+  return 0;
+}
+
+int printHelp() {
   cout << "Run with one of the following arguments:" << endl
     << "  setup-screen" << endl
     << "  picture" << endl
     << "  input" << endl;
+
+  return 0;
 }
 
 int main(int argc, char* argv[]) {
@@ -78,14 +128,14 @@ int main(int argc, char* argv[]) {
     char* arg = argv[1];
 
     if (strcmp(arg, "setup-screen") == 0)
-      testSetupScreen();
+      return testSetupScreen();
     else if (strcmp(arg, "picture") == 0)
-      testPicture();
+      return testPicture();
     else if (strcmp(arg, "input") == 0)
-      testInput();
+      return testInput();
+    else if (strcmp(arg, "blit") == 0)
+      return testBlit();
     else
-      printHelp();
+      return printHelp();
   }
-
-  return 0;
 }
